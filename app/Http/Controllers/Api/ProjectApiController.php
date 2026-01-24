c786df40a91f71d80a359e96edc3248c407144df
Daniel Mourad <danysarah2004@hotmail.com>
2026-01-24 15:56:50 +1100

    Implement public read-only projects and testimonials API

--- app/Http/Controllers/Api/ProjectApiController.php
+++ app/Http/Controllers/Api/ProjectApiController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectApiController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        // Paginate so you don’t dump everything if you add lots of projects later.
+        $projects = Project::query()
+            ->latest()
+            ->paginate(10);
+
+        return ProjectResource::collection($projects);
+    }
+
+    public function show(Project $project)
+    {
+        return new ProjectResource($project);
+    }
 }
--- app/Http/Controllers/Api/ProjectTestimonialApiController.php
+++ app/Http/Controllers/Api/ProjectTestimonialApiController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\TestimonialResource;
+use App\Models\Project;
 
 class ProjectTestimonialApiController extends Controller
 {
-    //
+    public function index(Project $project)
+    {
+        // Use the relationship you already have: Project hasMany Testimonials
+        $testimonials = $project->testimonials()
+            ->latest()
+            ->get();
+
+        return TestimonialResource::collection($testimonials);
+    }
 }
