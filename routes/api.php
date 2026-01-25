6eade2ca4979a036bde59ea4286b3288943fb7a3
Daniel Mourad <danysarah2004@hotmail.com>
2026-01-25 20:03:33 +1100

    test: verify sanctum auth with admin me endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectApiController;
 use App\Http\Controllers\Api\ProjectTestimonialApiController;
+use App\Http\Controllers\Api\Admin\AuthTokenController;
+
 
 Route::get('/projects', [ProjectApiController::class, 'index']);
 Route::get('/projects/{project}', [ProjectApiController::class, 'show']);
 Route::get('/projects/{project}/testimonials', [ProjectTestimonialApiController::class, 'index']);
+
+
+Route::prefix('admin')->group(function () {
+    // Login: get a token
+    Route::post('/token', [AuthTokenController::class, 'store']);
+
+    // Logout: revoke current token (requires being logged in via token)
+    Route::middleware('auth:sanctum')->delete('/token', [AuthTokenController::class, 'destroy']);
+});
